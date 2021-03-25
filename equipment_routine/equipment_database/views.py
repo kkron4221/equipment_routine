aadb24bd2cc18f5fa47cd1cbe604a498ddeee388
kkron4221 <knrro1877887@gmail.com>
2021-03-25 10:02:56 +0900

    Create templates function in equipment_database

--- equipment_routine/equipment_database/views.py
+++ equipment_routine/equipment_database/views.py
@@ -1,9 +1,14 @@
 from django.shortcuts import HttpResponse
 from django.views import generic
+from django.template import loader
 
 from .models import Equipment
 
-class DatabaseView(generic.DateDetailView):
-    def get_queryset(self):
-        return Equipment.objects.order_by('equipment_name')
-    template_name = '../equipment_database/' 
+def index(request):
+    template = loader.get_template('equipment_database/index.html')
+    return HttpResponse(template.render())
+
+# class DatabaseView(generic.DateDetailView):
+    # def get_queryset(self):
+    #     return Equipment.objects.order_by('equipment_name')
+    # template_name = '../equipment_database/' 
