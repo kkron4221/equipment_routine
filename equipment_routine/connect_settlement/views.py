37df7d7e3306fb19a631705f6c9b4fe915263af9
kkron4221 <knrro1877887@gmail.com>
2021-03-29 08:15:06 +0900

    Fixed html files

--- equipment_routine/connect_settlement/views.py
+++ equipment_routine/connect_settlement/views.py
@@ -4,4 +4,3 @@ from django.template import loader
 def display(request):
     template = loader.get_template('connect_settlement/this_month.html')
     return HttpResponse(template.render())
-    # return HttpResponse("hogehogwe")
--- equipment_routine/equipment_routine/settings.py
+++ equipment_routine/equipment_routine/settings.py
@@ -33,7 +33,6 @@ ALLOWED_HOSTS = []
 INSTALLED_APPS = [
     'equipment_database',
     'connect_settlement',
-    'equipment_database.apps.EquipmentDatabaseConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
