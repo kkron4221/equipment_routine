3eed9e938b90bc352d0e72666f5a841eceef279b
kkron4221 <knrro1877887@gmail.com>
2021-03-31 09:41:47 +0900

    Create equipment table in html

--- equipment_routine/equipment_database/views.py
+++ equipment_routine/equipment_database/views.py
@@ -1,11 +1,16 @@
 from django.shortcuts import HttpResponse
 from django.template import loader
+from django.shortcuts import render
+from .models import Equipment
 
 
 
 def index(request):
-    template = loader.get_template('equipment_database/index.html')
-    return HttpResponse(template.render())
+    equipments = Equipment.objects.order_by('equipment_amount')[0:]
+    context = {
+        'equipment_list': Equipment.objects.all(), 
+    }
+    return render(request, 'equipment_database/index.html', context)
 
 # class DatabaseView(generic.DateDetailView):
     # def get_queryset(self):
